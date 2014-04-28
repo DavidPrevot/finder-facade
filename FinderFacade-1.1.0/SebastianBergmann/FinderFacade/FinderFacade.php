4babf86c89caac7fd87a00980d56cc364c28e20c
David Prévot <taffit@debian.org>
2014-04-28 19:16:22 -0400

    Imported Upstream version 1.1.0

--- FinderFacade-1.1.0/SebastianBergmann/FinderFacade/FinderFacade.php
+++ FinderFacade-1.1.0/SebastianBergmann/FinderFacade/FinderFacade.php
@@ -43,102 +43,108 @@
 
 namespace SebastianBergmann\FinderFacade
 {
-    use TheSeer\fDOM\fDOMDocument;
+    use Symfony\Component\Finder\Finder;
 
     /**
-     * <code>
-     * <fileset>
-     *   <include>
-     *    <directory>/path/to/directory</directory>
-     *    <file>/path/to/file</file>
-     *   </include>
-     *   <exclude>/path/to/directory</exclude>
-     *   <name>*.php</name>
-     * </fileset>
-     * </code>
+     * Convenience wrapper for Symfony's Finder component.
      *
      * @author    Sebastian Bergmann <sebastian@phpunit.de>
      * @copyright 2012-2013 Sebastian Bergmann <sebastian@phpunit.de>
      * @license   http://www.opensource.org/licenses/BSD-3-Clause  The BSD 3-Clause License
-     * @version   Release: @package_version@
+     * @version   Release: 1.1.0
      * @link      http://github.com/sebastianbergmann/finder-facade/tree
      * @since     Class available since Release 1.0.0
      */
-    class Configuration
+    class FinderFacade
     {
         /**
-         * @var string
+         * @var array
          */
-        protected $basePath;
+        protected $items = array();
 
         /**
-         * @var fDOMDocument
+         * @var array
          */
-        protected $xml;
+        protected $excludes = array();
+
+        /**
+         * @var array
+         */
+        protected $names = array();
+
+
+        /**
+         * @var array
+         */
+        protected $notNames = array();
+
+        /**
+         * @param array $items
+         * @param array $excludes
+         * @param array $names
+         * @param array $notNames
+         */
+        public function __construct(array $items = array(), array $excludes = array(), array $names = array(), array $notNames = array())
+        {
+            $this->items    = $items;
+            $this->excludes = $excludes;
+            $this->names    = $names;
+            $this->notNames = $notNames;
+        }
+
+        /**
+         * @return array
+         */
+        public function findFiles()
+        {
+            $files   = array();
+            $finder  = new Finder;
+            $iterate = FALSE;
+
+            foreach ($this->items as $item) {
+                if (!is_file($item)) {
+                    $finder->in($item);
+                    $iterate = TRUE;
+                }
+
+                else {
+                    $files[] = realpath($item);
+                }
+            }
+
+            foreach ($this->excludes as $exclude) {
+                $finder->exclude($exclude);
+            }
+
+            foreach ($this->names as $name) {
+                $finder->name($name);
+            }
+
+            foreach ($this->notNames as $notName) {
+                $finder->notName($notName);
+            }
+
+            if ($iterate) {
+                foreach ($finder as $file) {
+                    $files[] = $file->getRealpath();
+                }
+            }
+
+            return $files;
+        }
 
         /**
          * @param string $file
          */
-        public function __construct($file)
+        public function loadConfiguration($file)
         {
-            $this->basePath = dirname($file);
+            $configuration = new Configuration($file);
+            $configuration = $configuration->parse();
 
-            $this->xml = new fDOMDocument;
-            $this->xml->load($file);
-        }
-
-        /**
-         * @param  string $xpath
-         * @return array
-         */
-        public function parse($xpath = '')
-        {
-            $result = array(
-              'items' => array(), 'excludes' => array(), 'names' => array(), 'notNames' => array()
-            );
-
-            foreach ($this->xml->getDOMXPath()->query($xpath . 'include/directory') as $item) {
-                $result['items'][] = $this->toAbsolutePath($item->nodeValue);
-            }
-
-            foreach ($this->xml->getDOMXPath()->query($xpath . 'include/file') as $item) {
-                $result['items'][] = $this->toAbsolutePath($item->nodeValue);
-            }
-
-            foreach ($this->xml->getDOMXPath()->query($xpath . 'exclude') as $exclude) {
-                $result['excludes'][] = $exclude->nodeValue;
-            }
-
-            foreach ($this->xml->getDOMXPath()->query($xpath . 'name') as $name) {
-                $result['names'][] = $name->nodeValue;
-            }
-
-            foreach ($this->xml->getDOMXPath()->query($xpath . 'notName') as $name) {
-                $result['notNames'][] = $name->nodeValue;
-            }
-
-            return $result;
-        }
-
-        /**
-         * @param  string $path
-         * @return string
-         */
-        protected function toAbsolutePath($path)
-        {
-            // Check whether the path is already absolute.
-            if ($path[0] === '/' || $path[0] === '\\' ||
-                (strlen($path) > 3 && ctype_alpha($path[0]) &&
-                 $path[1] === ':' && ($path[2] === '\\' || $path[2] === '/'))) {
-                return $path;
-            }
-
-            // Check whether a stream is used.
-            if (strpos($path, '://') !== FALSE) {
-                return $path;
-            }
-
-            return $this->basePath . DIRECTORY_SEPARATOR . $path;
+            $this->items    = $configuration['items'];
+            $this->excludes = $configuration['excludes'];
+            $this->names    = $configuration['names'];
+            $this->notNames = $configuration['notNames'];
         }
     }
 }
